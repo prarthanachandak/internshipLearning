5c5c7025425ce11f68cdcae89a7178e34b95c84b
Ganesh Birajdar <89690784+ganeshbirajdar09@users.noreply.github.com>
2022-02-08 23:44:55 +0530

    user model

--- login/models/user.js
+++ login/models/user.js
@@ -1,36 +1,35 @@
-//models - structure to define schema
+  //models - structure to define schema
 
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    default: 0,
-    unique: true
-  },
-  first_name: {
-    type: String,
-    default: null
-  },
-  last_name: {
-    type: String,
-    default: null
-  },
-  email: {
-    type: String,
-    unique: true
-  },
-  password: {
-    type: String
-  },
-  token: {
-    type: String
-  },
-  role: {
+  const userSchema = new mongoose.Schema({
+    // id: {
+    //   type: Number,
+    //   default: 0,
+    //   unique: true
+    // },
+    first_name: {
+      type: String,
+      default: null,
+    },
+    last_name: {
+      type: String,
+      default: null,
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    token: {
+      type: String,
+    },
+    role: {
       type: Number,
-      default: 0
-  }
-  
-});
+      default: 0,
+    },
+  });
 
-module.exports = mongoose.model("user", userSchema);
+  module.exports = mongoose.model("user", userSchema);
